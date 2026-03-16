3fe91f54f51e1c1ce1f7ede79040b537957dc86e
Kaylan Windryksson <kaylanwindryksson@gmail.com>
2026-03-15 23:41:35 -0300

    ajuste

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 import os
+import traceback
 from app import create_app
 
-app = create_app()
+try:
+    app = create_app()
+    print("App criado com sucesso!")
+except Exception as e:
+    print("ERRO CRÍTICO NA CRIAÇÃO DO APP:")
+    print(traceback.format_exc())
+    raise e # Isso vai forçar o Gunicorn a mostrar o erro no log
 
 if __name__ == '__main__':
-    # Em produção (Railway), ele usará a porta que o sistema definir
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
